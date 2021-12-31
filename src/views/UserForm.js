93bf5d2cb2eaf115859ce1401cb2f460e0dedd89
Joao Ferigato <joaoferigato@gmail.com>
2021-12-31 11:34:56 -0300

    Implementing UserForm +Context API

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,49 @@
 import React, { useState } from 'react'
-import { Text } from 'react-native'
+import { Button, Text, TextInput, View, StyleSheet } from 'react-native'
 
 export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>{user.id}</Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder='Informe o Nome'
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder='Informe o E-mail'
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder='Informe o E-mail'
+                value={user.avatarUrl}
+            />
+            <Button
+                title='Salvar'
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, FlatList, Alert } from 'react-native'
 import { Avatar, Icon, ListItem } from 'react-native-elements'
-
-import users from '../data/users'
+import UsersContext from '../context/UsersContext'
 
 export default props => {
+
+    const { state } = useContext(UsersContext)
 
     function confirmUserDeletion(user) {
         Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
@@ -53,7 +54,7 @@ export default props => {
         <View>
             <FlatList
                 keyExtractor={user => user.id.toString()}
-                data={users}
+                data={state.users}
                 renderItem={getUserItem}
             />
         </View>
