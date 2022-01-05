7a3df771349f245d4fcb27b1f808ae07433ae99c
Joao Ferigato <joaoferigato@gmail.com>
2022-01-05 13:58:05 -0300

    Adding useReducer hook

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -5,14 +5,17 @@ import UsersContext from '../context/UsersContext'
 
 export default props => {
 
-    const { state } = useContext(UsersContext)
+    const { state, dispatch } = useContext(UsersContext)
 
     function confirmUserDeletion(user) {
         Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
             {
                 text: 'Sim',
                 onPress() {
-                    console.warn('delete ' + user.id)
+                    dispatch({
+                        type: 'deleteUser',
+                        payload: user,
+                    })
                 }
             },
             {
