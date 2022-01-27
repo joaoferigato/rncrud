97927c18a41d7afbcbcf41021541729b3424d7b9
Joao Ferigato <joaoferigato@gmail.com>
2022-01-27 13:55:36 -0300

    Final commit

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,34 +1,41 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Text, TextInput, View, StyleSheet } from 'react-native'
+import UsersContext from '../context/UsersContext'
 
 export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
+    const { dispatch } = useContext(UsersContext)
+
     return (
         <View style={style.form}>
             <Text>Nome</Text>
             <TextInput
                 style={style.input}
-                onChangeText={name => setUser({...user, name})}
+                onChangeText={name => setUser({ ...user, name })}
                 placeholder='Informe o Nome'
                 value={user.name}
             />
             <Text>Email</Text>
             <TextInput
                 style={style.input}
-                onChangeText={email => setUser({...user, email})}
+                onChangeText={email => setUser({ ...user, email })}
                 placeholder='Informe o E-mail'
                 value={user.email}
             />
             <Text>URL do Avatar</Text>
             <TextInput
                 style={style.input}
-                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
                 placeholder='Informe o E-mail'
                 value={user.avatarUrl}
             />
             <Button
                 title='Salvar'
                 onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user,
+                    })
                     navigation.goBack()
                 }}
             />
